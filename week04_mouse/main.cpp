11366ecf8c4a54fd7efdd072a4ebbee4bf4b69ac
unknown <cyxx0610@gmail.com>
2022-03-14 16:52:22 +0800

    week04

---
+++ week04_mouse/main.cpp
@@ -0,0 +1,23 @@
+#include <GL/glut.h>
+#include <stdio.h>
+void display()
+{
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glutSwapBuffers();
+}
+void mouse (int button , int state, int x, int y)
+{
+    printf("%d %d %d %d\n",button,state,x,y);
+}
+int main(int argc, char**argv)
+{
+    glutInit( &argc,argv);
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH );
+    glutCreateWindow("week03");
+
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMainLoop();
+    return 0;
+}
